de10f75d9b9375815efb59e296dc575e55c17168
Nick Aguilar <naguilar9404@conestogac.on.ca>
2024-03-23 15:48:33 -0400

    finished loading more posts with a button click module

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -9,9 +9,8 @@ from .views import (
 app_name = 'posts'
 
 urlpatterns = [
-    path('', post_list_and_create, name='main-board'),
-    
-    path('data/', load_post_data_view, name='post-data'),
+    path('', post_list_and_create, name='main-board'),    
+    path('data/<int:num_posts>/', load_post_data_view, name='post-data'),
     
     path('hello-world/', hello_world_view, name='hello-world'),
 ]
