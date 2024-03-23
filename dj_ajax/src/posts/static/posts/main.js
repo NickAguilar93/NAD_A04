907c5f05f2534c82d91cc0ad7e4e5d679cad25b4
Nick Aguilar <naguilar9404@conestogac.on.ca>
2024-03-23 14:20:09 -0400

    Finished First Ajax Call module

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -2,4 +2,17 @@ console.log("Hello World");
 
 const helloWorldBox = document.getElementById('hello-world');
 
-helloWorldBox.innerHTML = 'Hello <b>World</b>'
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response) {
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error) {
+        console.log('error', error)
+        helloWorldBox.textContent = error.text
+    },
+})
