bd43b2871945e6837ce137c114d2e2b32b3d59ab
Nick Aguilar <naguilar9404@conestogac.on.ca>
2024-03-24 19:05:56 -0400

    Fixing save

--- dj_ajax/src/profiles/views.py
+++ dj_ajax/src/profiles/views.py
@@ -23,26 +23,3 @@ def my_profile_view(request):
         
         
     return render(request, 'profiles/main.html', context)
-
-def my_profile_view(request):
-    obj = Profile.objects.get(user=request.user)
-    form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        if form.is_valid():
-            instance = form.save()
-            return JsonResponse({
-                'bio': instance.bio,
-                'avatar': instance.avatar.url if instance.avatar else None,  # Check if avatar exists
-                'user': instance.user.username
-            })
-        else:
-            # In case form is not valid and it's an AJAX request, you might want to return errors
-            return JsonResponse({'errors': form.errors}, status=400)
-    
-    # This part will handle non-AJAX requests or AJAX requests where the form is not valid
-    context = {
-        'obj': obj,
-        'form': form,
-    }
-    
-    return render(request, 'profiles/main.html', context)
