b5b734d3c471e1026e222d280637525111711bf5
Nick Aguilar <naguilar9404@conestogac.on.ca>
2024-03-24 12:48:42 -0400

    Finished 'Working on the Post Detail Page - Part 2' module

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -4,6 +4,7 @@ from .views import (
     load_post_data_view,
     like_unlike_post,
     post_detail,
+    post_detail_data_view,
     
 )
 
@@ -14,7 +15,6 @@ urlpatterns = [
     path('like-unlike/', like_unlike_post, name='like-unlike'), 
     path('<pk>/', post_detail, name='post-detail'),
        
-    path('data/<int:num_posts>/', load_post_data_view, name='post-data'),
-    
-
+    path('data/<int:num_posts>/', load_post_data_view, name='post-data'), 
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
 ]
