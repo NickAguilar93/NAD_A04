9353225e1ed5d25d6b8000c344ef84a23c004a50
Nick Aguilar <naguilar9404@conestogac.on.ca>
2024-04-05 13:45:54 -0400

    checkpoint

--- src/posts_proj/urls.py
+++ src/posts_proj/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
-    path('profiles/', include('profiles.urls', namespace='profiles'))
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
